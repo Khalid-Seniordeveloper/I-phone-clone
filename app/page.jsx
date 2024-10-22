d566c0de4909373820a57afe83c062698aba5c0c
Khalid-Seniordeveloper <kb171748@gmail.com>
2024-10-23 02:12:39 +0500

    done

--- app/page.jsx
+++ app/page.jsx
@@ -28,7 +28,7 @@ import subtwo from './assets/subtwo.png'
 import subthree from './assets/subthree.png'
 import subfour from './assets/subfour.png'
 import banner from './assets/banner.png'
-
+import flogo from "./assets/flogo.png"
 import { CiHeart } from "react-icons/ci";
 const page = () => (
   <>
@@ -278,11 +278,11 @@ const page = () => (
 <div className="flex justify-center items-center mt-12"><Image src={galaxyimage} className="w-56 product"/></div>
 <div className="w-[100%] flex justify-center items-center mt-5">
 
-<h1 className="text-2xl text-center product-name">Apple Iphone 14 Pro Max <br /> 128GB Deep Purple </h1>
+<h1 className="text-2xl text-center product-name">Blackmagic Pocket Cinema <br /> Camera 6k </h1>
 
 </div>
 <div className="w-[100%] flex justify-center items-center">
-<span className="text-3xl mt-6 price"><b>$900</b></span>
+<span className="text-3xl mt-6 price"><b>$2535</b></span>
 </div>
 <div className="w-[100%] mt-7 buy-btn flex justify-center items-center">
   <button>Buy Now</button>
@@ -468,6 +468,66 @@ const page = () => (
 <div className="bacground-image-container">
   <Image src={banner} className="banner"/>
 </div>
+
+{/* footer code start  */}
+
+<div className="footer">
+
+  
+<div className="footer-main-container flex flex-wrap justify-between  ">
+
+<div className="mt-[3rem]" >
+<ul className="flex flex-col gap-12 justify-center">
+
+<li><Image src={flogo} className="footer-logo"/></li>
+<li className="description-footer text-2xl">We are a residential interior design firm located in Portland. <br /> Our boutique-studio offers more than</li>
+</ul>
+</div>
+
+<div className="mt-[3rem]" >
+<ul className="flex flex-col gap-5 justify-center">
+
+<li><h1 className="text-[2.3rem] text-white">Services</h1></li>
+<li className="description-footer text-2xl">Bonus Programs</li>
+<li className="description-footer text-2xl">Gifts Cards</li>
+<li className="description-footer text-2xl">Credits And Payments</li>
+<li className="description-footer text-2xl">Services Contracts</li>
+<li className="description-footer text-2xl">Non Crash Account</li>
+<li className="description-footer text-2xl">Payment</li>
+
+
+</ul>
+</div>
+
+<div className="mt-[3rem]" >
+<ul className="flex flex-col gap-5 justify-center">
+
+<li><h1 className="text-[2.3rem] text-white">Assistance to the buyer</h1></li>
+<li className="description-footer text-2xl">Find an Order</li>
+<li className="description-footer text-2xl">Term of delivery</li>
+<li className="description-footer text-2xl">Exchange and return</li>
+<li className="description-footer text-2xl">Guarantee</li>
+<li className="description-footer text-2xl">Frequently asked questions</li>
+<li className="description-footer text-2xl">Terms of use of the site</li>
+
+</ul>
+</div>
+
+
+</div>
+
+
+
+
+
+</div>
+<div className="w-[100%] my flex justify-center items-center">
+  <h1 className="text-2xl">Complete responsive developed by Muhammad Khalid GiAiC Sunday Morning (Sir Anas)</h1>
+</div>
+
+
+
+
 
   </div>
  
